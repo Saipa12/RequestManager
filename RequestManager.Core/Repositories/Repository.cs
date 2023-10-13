5476a3b9ed05eb191c66817d97157dda209274e1
konstantinikolaevi <105286656+konstantinikolaevi@users.noreply.github.com>
2023-10-13 15:45:02 +0300

    Добавил возможность не сохранять изменения в Repository

--- RequestManager.Core/Repositories/Repository.cs
+++ RequestManager.Core/Repositories/Repository.cs
@@ -25,53 +25,53 @@ public abstract class Repository<TEntity> : IRepository where TEntity : class
 
     public virtual async Task<IEnumerable<TEntity>> GetAsync(Expression<Func<TEntity, bool>> predicate) => await _databaseContext.Set<TEntity>().Where(predicate).ToListAsync();
 
-    public virtual async Task<TEntity> CreateAsync(TEntity entity)
+    public virtual async Task<TEntity> CreateAsync(TEntity entity, bool saveChanges = true)
     {
         await _databaseContext.Set<TEntity>().AddAsync(entity);
-        return await SaveAndDetachAsync(entity);
+        return await SaveAndDetachAsync(entity, saveChanges);
     }
 
-    public virtual async Task<IEnumerable<TEntity>> CreateAsync(IEnumerable<TEntity> entities)
+    public virtual async Task<IEnumerable<TEntity>> CreateAsync(IEnumerable<TEntity> entities, bool saveChanges = true)
     {
         await _databaseContext.Set<TEntity>().AddRangeAsync(entities);
-        return await SaveAndDetachAsync(entities);
+        return await SaveAndDetachAsync(entities, saveChanges);
     }
 
-    public virtual async Task<TEntity> UpdateAsync(TEntity entity)
+    public virtual async Task<TEntity> UpdateAsync(TEntity entity, bool saveChanges = true)
     {
         //_databaseContext.Entry(entity).State = EntityState.Unchanged; // TODO
         _databaseContext.Update(entity);
-        return await SaveAndDetachAsync(entity);
+        return await SaveAndDetachAsync(entity, saveChanges);
     }
 
-    public virtual async Task<IEnumerable<TEntity>> UpdateAsync(IEnumerable<TEntity> entities)
+    public virtual async Task<IEnumerable<TEntity>> UpdateAsync(IEnumerable<TEntity> entities, bool saveChanges = true)
     {
         //_databaseContext.AttachRange(entities); // TODO
         _databaseContext.UpdateRange(entities);
-        return await SaveAndDetachAsync(entities);
+        return await SaveAndDetachAsync(entities, saveChanges);
     }
 
-    public virtual async Task<TEntity> DeleteAsync(TEntity entity)
+    public virtual async Task<TEntity> DeleteAsync(TEntity entity, bool saveChanges = true)
     {
         //_databaseContext.Entry(entity).State = EntityState.Deleted; // TODO
         _databaseContext.Remove(entity);
-        return await SaveAndDetachAsync(entity);
+        return await SaveAndDetachAsync(entity, saveChanges);
     }
 
-    public virtual async Task<IEnumerable<TEntity>> DeleteAsync(IEnumerable<TEntity> entities)
+    public virtual async Task<IEnumerable<TEntity>> DeleteAsync(IEnumerable<TEntity> entities, bool saveChanges = true)
     {
         _databaseContext.RemoveRange(entities);
-        return await SaveAndDetachAsync(entities);
+        return await SaveAndDetachAsync(entities, saveChanges);
     }
 
-    protected async Task<TEntity> SaveAndDetachAsync(TEntity entity)
+    protected async Task<TEntity> SaveAndDetachAsync(TEntity entity, bool saveChanges = true)
     {
         await _databaseContext.SaveChangesAsync();
         _databaseContext.Entry(entity).State = EntityState.Detached;
         return entity;
     }
 
-    protected async Task<IEnumerable<TEntity>> SaveAndDetachAsync(IEnumerable<TEntity> entities)
+    protected async Task<IEnumerable<TEntity>> SaveAndDetachAsync(IEnumerable<TEntity> entities, bool saveChanges = true)
     {
         await _databaseContext.SaveChangesAsync();
         _databaseContext.DetachRange(entities);
